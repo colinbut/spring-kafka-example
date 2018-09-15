4f46345ba81cde80923c11b3dad6a1dc7a7bddff
Colin But <colinbut@users.noreply.github.com>
2018-09-15 17:23:27 +0100

    setup Kafka configuration in Spring

---
+++ src/main/java/com/mycompany/spring/kafka/springkafkaexample/config/KafkaConfig.java
@@ -0,0 +1,15 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.spring.kafka.springkafkaexample.config;
+
+public final class KafkaConfig {
+
+    private KafkaConfig(){}
+
+    public static final String TOPIC_NAME = "spring.kafka.topic";
+
+    public static final String GROUP_ID = "1";
+}
---
+++ src/main/java/com/mycompany/spring/kafka/springkafkaexample/config/KafkaConsumerConfig.java
@@ -0,0 +1,49 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.spring.kafka.springkafkaexample.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "bootstrap-address");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, KafkaConfig.GROUP_ID);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> filterKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setRecordFilterStrategy(consumerRecord -> consumerRecord.value().contains("Sex"));
+        return factory;
+    }
+
+}
